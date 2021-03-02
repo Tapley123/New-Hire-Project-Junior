1d4592bcc61eaf44a4a9eb29642353eebba6e476
Tapley123 <conortapley1@gmail.com>
2021-03-02 20:12:40 +0000

    Level Setup and player Movement

---
+++ New Hire Project - Junior/Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        Launch();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void Launch()
+    {
+        float x = Random.Range(0, 2) * 2 - 1; //randomly = -1 or 1;
+        float y = Random.Range(0, 2) * 2 - 1; //randomly = -1 or 1;
+
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+}
---
+++ New Hire Project - Junior/Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1 = false;
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
+    private float movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if(isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+}
