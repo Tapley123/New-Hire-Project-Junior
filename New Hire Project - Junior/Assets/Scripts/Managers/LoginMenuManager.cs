56c10cdadea76e059b1245eef31914423506e2bf
Tapley123 <conortapley1@gmail.com>
2021-03-16 18:09:43 +0000

    Error Messages displayed on screen

--- New Hire Project - Junior/Assets/Scripts/Managers/LoginMenuManager.cs
+++ New Hire Project - Junior/Assets/Scripts/Managers/LoginMenuManager.cs
@@ -9,6 +9,10 @@ public class LoginMenuManager : MonoBehaviour
     public GameObject loginPanel;
     public GameObject registerPanel;
 
+    [Header("Display Text")]
+    public TMP_Text messageText;
+    public TMP_Text errorText;
+
     [Header ("Creating Player")]
     public TMP_InputField displayNameInputCreating;
     public TMP_InputField userNameInputCreating;
@@ -50,10 +54,14 @@ public class LoginMenuManager : MonoBehaviour
     public void B_GoToLoginScreen()
     {
         GoToLoginScreen();
+        messageText.text = "";
+        errorText.text = "";
     }
     public void B_GoToRegisterScreen()
     {
         GoToRegisterScreen();
+        messageText.text = "";
+        errorText.text = "";
     }
 
     //Registers a player on the gamesparks server based on the username and password input
@@ -62,6 +70,8 @@ public class LoginMenuManager : MonoBehaviour
         if(displayNameInputCreating.text != "" && userNameInputCreating.text != "" && passwordInputCreating.text != "") //check if the input fields are not empty
         {
             Debug.Log("Registering Player...");
+            messageText.text = "Registering Player...";
+            errorText.text = "";
 
             new GameSparks.Api.Requests.RegistrationRequest()
             .SetDisplayName(displayNameInputCreating.text)
@@ -72,23 +82,33 @@ public class LoginMenuManager : MonoBehaviour
                 if (!response.HasErrors)
                 {
                     Debug.Log("Player Registered \n User Name: " + response.DisplayName);
+                    messageText.text = "Player Registered \n User Name: " + response.DisplayName;
+                    errorText.text = "";
                     GoToLoginScreen();
                 }
                 else
                 {
                     //display all errors from registering player
                     Debug.Log("Error Registering Player... \n " + response.Errors.JSON.ToString());
+                    errorText.text = "Error Registering Player... \n " + response.Errors.JSON.ToString();
+                    messageText.text = "";
                 }
             });
         }
         else
+        {
             Debug.Log("Error Registering Player... Make sure you have filled in all of the boxes");
+            errorText.text = "Error Registering Player... Make sure you have filled in all of the boxes";
+            messageText.text = "";
+        }
     }
 
     //checks the username and password a player has input and if they are correct then the player logs in
     public void B_Login()
     {
         Debug.Log("Autorising Player...");
+        messageText.text = "Autorising Player...";
+        errorText.text = "";
 
         new GameSparks.Api.Requests.AuthenticationRequest()
             .SetUserName(userNameInputLogin.text)
@@ -98,12 +118,16 @@ public class LoginMenuManager : MonoBehaviour
                 if (!response.HasErrors)
                 {
                     Debug.Log("Player Authenticated \n User Name: " + response.DisplayName);
+                    messageText.text = "Player Authenticated \n User Name: " + response.DisplayName;
+                    errorText.text = "";
                     LoadGameScene();
                 }
                 else
                 {
                     //display all errors from registering player
                     Debug.Log("Error Authenticating Player... \n " + response.Errors.JSON.ToString());
+                    errorText.text = "Error Authenticating Player... \n " + response.Errors.JSON.ToString();
+                    messageText.text = "";
                 }
             });
     }
